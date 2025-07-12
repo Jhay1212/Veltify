97292a370e15369f61bfd2db61e60daee8b9ffb3
Jhay12 <jhayjane78@gmail.com>
2025-07-12 23:03:45 +0800

    testing seeding

---
+++ resources/js/components/VerticalNav.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="w-1/5 top-0 z-22 bg-lime-500 h-screen relative left-0" >
+        <!-- user part -->
+         <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center">
+                <div class="flex-justify-around text-center ">
+                    <img src="https://images.icon-icons.com/1378/PNG/512/avatardefault_92824.png" class="rounded-full h-36  w-36" alt="" srcset="">
+                    <h2 className="text-xl font-bold ">Current User</h2>
+                    <ul class="flex flex-col gap-2">
+                        <li><a href="#" className="text-lg   hover:underline underline-offset-10">Home</a></li>
+                        <li><a href="#" className="text-lg   hover:underline underline-offset-10">About</a></li>
+                        <li><a href="#" className="text-lg   hover:underline underline-offset-10">Contact</a></li>
+                    </ul>
+
+                </div>
+            </div>
+            </div>
+    </div>
+</template>
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,8 +1,11 @@
 
 <script setup lang="ts">
 import Navbar from '@/components/Navbar.vue';
+import VerticalNav from '@/components/VerticalNav.vue';
 </script>
 <template>
+    <VerticalNav />
     <Navbar />
+
     <h1 class="text-3xl text-white font-bold underline">Hello world!</h1>
 </template>
